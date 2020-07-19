96b7aa4240ba380c4b4463c04181da2f2f55cb4d
amenoyoya <mtyswe3@gmail.com>
2020-07-19 21:15:06 +0900

    🐛 fix: docker構成修正

--- www/startup.sh
+++ www/startup.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
-# -- www-data@docker://web/
+# -- sudo www-data@docker://web/
 
 # 環境変数 UID が与えられていれば www-data ユーザIDを $UID に合わせる
 if [ "$UID" != "" ]; then
     # uid = $UID のダミーユーザ作成
-    sudo useradd -u $UID -g $UID myuser
+    if [ "$(getent passwd $UID)" = "" ]; then
+        useradd -u $UID -g $UID myuser
+    fi
     # www-data ユーザIDを変更
-    sudo usermod -u $UID www-data
-    sudo groupmod -g $UID www-data
+    usermod -u $UID www-data
+    groupmod -g $UID www-data
     # www-data のホームディレクトリのパーミッション修正
-    sudo chown -R www-data:www-data /var/www/
+    chown -R www-data:www-data /var/www/
 fi
 
 # ~/.msmtprc のパーミッション修正
-sudo chown www-data:www-data /var/www/.msmtprc
-sudo chmod 600 /var/www/.msmtprc
+chown www-data:www-data /var/www/.msmtprc
+chmod 600 /var/www/.msmtprc
 
 # プロジェクト準備: setup.sh を編集することで任意のテンプレートを準備可能
 if [ ! -d './app' ]; then
     source ./setup.sh
 fi
 
-# 環境変数を引き継いで Apache を起動させる
-sudo -E apachectl -D FOREGROUND
+# Apache をフォアグランド起動
+apachectl -D FOREGROUND
